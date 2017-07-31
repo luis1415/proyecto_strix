9a8ecfd0c2d6594c6cca051930184832b2c9942b
luis1415 <luisbermudeztorres@gmail.com>
2017-07-30 19:50:25 -0500

    modelo de pacientes

--- apps/pacientes/models.py
+++ apps/pacientes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Paciente(models.Model):
+    cedula = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    fecha_registro = models.DateField()
+    nombre_empresa = models.CharField(max_length=50)
+    tipo_certificado = models.CharField(max_length=100)
+    examenes_practicados = models.CharField(max_length=1000)
+    telefono = models.CharField(max_length=20)
+    celular = models.CharField(max_length=20)
+    ciudad = models.CharField(max_length=50)
+    fecha_nacimiento = models.DateField()
+    estado_civil = models.CharField(max_length=50)
+    escolaridad = models.CharField(max_length=50)
+    profesion = models.CharField(max_length=100)
